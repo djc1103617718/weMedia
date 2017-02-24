f28848418f91595ab94315fa51653a9c5c24704d
daniel <djc1103617718@163.com>
2017-02-24 16:19:08 +0800

    sql config

--- config/db.php
+++ config/db.php
@@ -2,9 +2,15 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=media',
+    /*'dsn' => 'mysql:host=localhost;dbname=media',
     'username' => 'root',
     'password' => 'asdf',
+    'charset' => 'utf8',
+    'tablePrefix' => 'm_',*/
+    'dsn' => 'mysql:host=172.16.0.181;dbname=media',
+    //'dsn' => 'mysql:host=172.16.0.129;dbname=media',
+    'username' => 'root',
+    'password' => '',
     'charset' => 'utf8',
     'tablePrefix' => 'm_',
 ];
